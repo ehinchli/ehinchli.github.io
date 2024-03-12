812edf86f2eb0a906eed1216efc0aa187bc27ded
Evan Hinchliffe <evanmhinchliffe@gmail.com>
2024-03-12 09:14:11 -0400

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: 'jit',
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+};
+
